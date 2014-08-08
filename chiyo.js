6ccd49f448895ba4ae8e1f5aecba36d39bd9ffd6
southrop <southrop113@gmail.com>
2014-08-08 18:54:47 +0800

    word wrapping implemented

--- chiyo.js
+++ chiyo.js
@@ -8,27 +8,62 @@ imageObj.onload = function() {
 };
 imageObj.src = 'http://puu.sh/aCUEX.jpg';
 
-var radians = 180 / Math.PI;
+var angle = -4/(180 / Math.PI);
 
 context.font = '3em Oswald';
 context.textAlign = 'center';
 
-
-
 document.getElementById('sexualize').addEventListener('keyup', function (){
 	writeText(context, this.value);
 }, false);
+
 
 function writeText(context, text) {
 	context.clearRect(0, 0, canvas.width, canvas.height); // Clear's canvas
     context.drawImage(imageObj, 0, 0); // redraws Chiyo
     
     context.save();
-    context.rotate(-4/radians); // rotates to match the sign board
+    context.rotate(angle); // rotates to match the sign board
 
-    context.fillText(text, 247, 391);
+    //context.fillText(text, 247, 391);
+    wordWrap(context, text, 247, 391);
 
     context.restore(); // undoes rotation
 }
 
-// TO DO: WORD WRAP
+// TODO: HANDLE TEXT OVERFLOW
+
+function wordWrap(context, text, x, y) {
+	context.font = '3em Oswald';
+	context.textAlign = 'center';
+
+	var lineHeight = 71;
+	var maxWidth = 230;
+
+	var words = text.split("\n");
+	for (var i = 0; i < words.length; i++) {
+		words[i] = words[i].split(" ");
+	};
+	console.log(words);
+
+	for (var i = 0; i < words.length; i++) {
+		var line = "";
+		for (var j = 0; j < words[i].length; j++) {
+			if(line == "") {
+				var testLine = line + words[i][j];
+			} else {
+				var testLine = line + " " + words[i][j];
+			}
+
+			if (context.measureText(testLine).width > maxWidth) {
+				context.fillText(line, x, y);
+				line = words[i][j];
+				y += lineHeight;
+			} else {
+				line = testLine;
+			}
+		};
+		context.fillText(line, x, y);
+		y += lineHeight;
+	};
+}
